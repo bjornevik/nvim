9760205faceb801218218ea427b1ce87160d68b4
John Even Bjørnevik <johnbjornevik@gmail.com>
2022-02-28 20:55:07 +0100

    add "readme" in init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,26 @@
+--[[
+
+    ____      ______  ____  _   _________    ________ __
+   / __ )    / / __ \/ __ \/ | / / ____/ |  / /  _/ //_/
+  / __  |_  / / / / / /_/ /  |/ / __/  | | / // // ,<
+ / /_/ / /_/ / /_/ / _, _/ /|  / /___  | |/ // // /| |
+/_____/\____/\____/_/ |_/_/ |_/_____/  |___/___/_/ |_|
+
+
+Notes to people reading my configuration
+
+This configuration runs on Neovim Nightly (NVIM v0.7.0-dev+1158)
+
+It uses the wbthomason/packer.nvim package manager.
+
+* the lua/plugins.lua file contains the code for the packer.nvim package manager
+* the lua/defaults/ dir contains my basic neovim options.
+* the lua/bjornevik/ dir contains all specialized setup with regards to plugins etc.
+
+I use <space> as my leader key.
+--]]
 vim.g.mapleader = " "
 
--- Packer.nvim
 require "plugins"
-
--- Neovim options
 require "defaults"
-
--- Plugins and stuff
 require "bjornevik"
