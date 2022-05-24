6e0b7f13b218d5ac23a1af667a2094bc19f8bd29
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-05-24 09:10:55 +0200

    feat(telescope_file_browser): enable hijack_netrw option

--- lua/bjornevik/telescope.lua
+++ lua/bjornevik/telescope.lua
@@ -72,6 +72,7 @@ telescope.setup {
     },
     file_browser = {
       hidden = true,
+      hijack_netrw = true,
       mappings = {
         ["i"] = {
           ["<M-n>"] = require("telescope").extensions.file_browser.actions.create_from_prompt,
