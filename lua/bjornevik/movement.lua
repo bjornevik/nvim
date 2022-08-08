d99301038544b72a00d9c7d6401cc9cec369d479
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-08-08 08:39:02 +0200

    chore: edit keybinds for smart-splits

--- lua/bjornevik/movement.lua
+++ lua/bjornevik/movement.lua
@@ -8,14 +8,14 @@ require("lightspeed").setup {
 local smart_splits, _ = pcall(require, "smart-splits")
 if smart_splits then
   -- resize splits
-  vim.keymap.set("n", "<C-M-H>", require("smart-splits").resize_left)
-  vim.keymap.set("n", "<C-M-J>", require("smart-splits").resize_down)
-  vim.keymap.set("n", "<C-M-K>", require("smart-splits").resize_up)
-  vim.keymap.set("n", "<C-M-L>", require("smart-splits").resize_right)
+  vim.keymap.set("n", "<M-Left>", require("smart-splits").resize_left)
+  vim.keymap.set("n", "<M-Down>", require("smart-splits").resize_down)
+  vim.keymap.set("n", "<M-Up>", require("smart-splits").resize_up)
+  vim.keymap.set("n", "<M-Right>", require("smart-splits").resize_right)
 
   -- moving between splits
-  vim.keymap.set("n", "<M-H>", require("smart-splits").move_cursor_left)
-  vim.keymap.set("n", "<M-J>", require("smart-splits").move_cursor_down)
-  vim.keymap.set("n", "<M-K>", require("smart-splits").move_cursor_up)
-  vim.keymap.set("n", "<M-L>", require("smart-splits").move_cursor_right)
+  vim.keymap.set("n", "<C-Left>", require("smart-splits").move_cursor_left)
+  vim.keymap.set("n", "<C-Down>", require("smart-splits").move_cursor_down)
+  vim.keymap.set("n", "<C-Up>", require("smart-splits").move_cursor_up)
+  vim.keymap.set("n", "<C-Right>", require("smart-splits").move_cursor_right)
 end
