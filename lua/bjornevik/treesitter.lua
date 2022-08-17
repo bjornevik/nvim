c6196f4bd88d1910a5b6380d729da033fc2b70f7
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-08-17 08:29:25 +0200

    chore: incremental selection treesitter

--- lua/bjornevik/treesitter.lua
+++ lua/bjornevik/treesitter.lua
@@ -15,30 +15,18 @@ require("nvim-treesitter.configs").setup {
   },
   incremental_selection = {
     enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_increamental = "grc",
+      node_decremental = "grm",
+    },
   },
   textobjects = {
     select = {
       enable = true,
       -- Automatically jump forward to textobj, similar to targets.vim
       lookahead = true,
-      move = {
-        enable = true,
-        set_jumps = true,
-
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-      },
       keymaps = {
         -- You can use the capture groups defined in textobjects.vim
         ["af"] = "@function.outer",
