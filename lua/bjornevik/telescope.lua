9532106c75041fe846838ce1c5b4c8c581ce36dd
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-10-07 10:53:20 +0200

    chore(telescope): keybinds

--- lua/bjornevik/telescope.lua
+++ lua/bjornevik/telescope.lua
@@ -108,19 +108,15 @@ end
 
 -- Telescope remaps
 vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { noremap = true })
+vim.keymap.set("n", "<leader>tb", telescope_builtin.buffers, { noremap = true })
+vim.keymap.set("n", "<leader>fs", telescope_builtin.live_grep, { noremap = true })
+vim.keymap.set("n", "<leader>bf", telescope_builtin.current_buffer_fuzzy_find, { noremap = true })
+vim.keymap.set("n", "<leader>fo", telescope_builtin.oldfiles, { noremap = true })
+vim.keymap.set("n", "<leader>fg", telescope_builtin.git_files, { noremap = true })
+vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { noremap = true })
+vim.keymap.set("n", "<leader>fr", telescope_builtin.lsp_references, { noremap = true })
 
 vim.keymap.set("n", "<leader>fb", telescope.extensions.file_browser.file_browser, { noremap = true })
-
-vim.keymap.set("n", "<leader>tb", telescope_builtin.buffers, { noremap = true })
-
-vim.keymap.set("n", "<leader>fs", telescope_builtin.live_grep, { noremap = true })
-
-vim.keymap.set("n", "<leader>bf", telescope_builtin.current_buffer_fuzzy_find, { noremap = true })
-
-vim.keymap.set("n", "<leader>fo", telescope_builtin.oldfiles, { noremap = true })
-
-vim.keymap.set("n", "<leader>fg", telescope_builtin.git_files, { noremap = true })
-
 vim.keymap.set("n", "<leader>vrc", M.search_dotfiles, { noremap = true })
 
 -- Plugin specific remaps
