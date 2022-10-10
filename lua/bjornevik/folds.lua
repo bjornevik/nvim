160ca5198ff5c098676d78450b843751ced43253
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-10-10 09:35:54 +0200

    ufo: more keymaps

--- lua/bjornevik/folds.lua
+++ lua/bjornevik/folds.lua
@@ -2,9 +2,18 @@ vim.o.foldcolumn = "0"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = "1"
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
+vim.keymap.set("n", "zk", function()
+  local winid = require("ufo").peekFoldedLinesUnderCursor()
+  if not winid then
+    vim.lsp.buf.hover()
+  end
+end)
 
 local virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
