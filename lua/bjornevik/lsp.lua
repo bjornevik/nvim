8a04480f802852822d200a9bc9115f90b2aec7a8
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-10-17 08:05:31 +0200

    cmp: update_capabilities deprecated

--- lua/bjornevik/lsp.lua
+++ lua/bjornevik/lsp.lua
@@ -48,7 +48,7 @@ local on_attach = function(client)
   end
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.colorProvier = {
   dynamicRegistration = true,
 }
