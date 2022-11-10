0815610a4fcf1fb81c85ba6f62ae6ec1b28e7375
John Even Bjørnevik <Johnbjornevik@gmail.com>
2022-11-10 10:21:15 +0100

    chore(alpha): reorder and add help tags

--- lua/bjornevik/appearance.lua
+++ lua/bjornevik/appearance.lua
@@ -95,10 +95,11 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
   dashboard.button("SPC f f", "  Find File", ":Telescope find_files<CR>"),
+  dashboard.button("SPC f s", "  Find String", ":Telescope live_grep<CR>"),
   dashboard.button("SPC f b", "  File Browser", ":Telescope file_browser<CR>"),
-  dashboard.button("SPC f o", "  Recent", ":Telescope oldfiles<CR>"),
-  dashboard.button("SPC f s", "  Find String", ":Telescope live_grep<CR>"),
   dashboard.button("SPC h m", "  Harpoon", ":lua require('harpoon.ui').toggle_quick_menu()<CR>"),
+  dashboard.button("SPC f h", "ﲉ  Help", ":Telescope help_tags<CR>"),
+  dashboard.button("SPC f o", "勒 Recent", ":Telescope oldfiles<CR>"),
   dashboard.button("SPC vrc", "  Settings", ":lua require('bjornevik.telescope').search_dotfiles()<CR>"),
 }
 
