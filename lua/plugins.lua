7b61865deb643ea01ca1e1900520e7d9c277d9ae
John Even Bjørnevik <Johnbjornevik@gmail.com>
2023-01-17 13:37:14 +0100

    chore: disable noice

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -113,10 +113,10 @@ return packer.startup(function(use)
   use "ThePrimeagen/harpoon"
 
   -- Appearance
-  use { "folke/noice.nvim", requires = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  } }
+  -- use { "folke/noice.nvim", requires = {
+  --   "MunifTanjim/nui.nvim",
+  --   "rcarriga/nvim-notify",
+  -- } }
   use "luisiacc/gruvbox-baby"
   use "goolord/alpha-nvim"
 
