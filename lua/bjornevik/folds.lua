874f1901960d9e1cdb2477f81b312c817ae85583
John Even Bjørnevik <Johnbjornevik@gmail.com>
2023-01-18 08:21:28 +0100

    chore: (disabled) statuscolumn update

--- lua/bjornevik/folds.lua
+++ lua/bjornevik/folds.lua
@@ -3,8 +3,12 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = "1"
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
--- vim.o.statuscolumn =
---   '%=%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "" : "") : " " }%r%s'
+-- vim.o.statuscolumn = "%s%=%l %{"
+--   .. "foldlevel(v:lnum) > foldlevel(v:lnum - 1)"
+--   .. "? foldclosed(v:lnum) == -1"
+--   .. '? ""'
+--   .. ': ""'
+--   .. ':" "}'
 
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
