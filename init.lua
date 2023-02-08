177156d4c1004add89c5eb1344b58c5e591ef09c
John Even Bjørnevik <Johnbjornevik@gmail.com>
2023-02-08 08:46:11 +0100

    revert to packer

--- init.lua
+++ init.lua
@@ -19,10 +19,11 @@ vim.g.mapleader = " "
 
 pcall(require, "impatient")
 
+require "plugins"
+
 -- COLORSCHEME
 vim.g.gruvbox_baby_use_original_palette = true
 vim.cmd.colorscheme "gruvbox-baby"
 
-require "plugins"
 require "defaults"
 require "bjornevik"
